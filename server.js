d37f0c79cc6734cbaed2d7f966b87aa1a1384898
Abdul Moeez <stuntsadv0987654@gmail.com>
2025-08-30 22:14:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-  origin: "http://localhost:5173/",
+  origin: true,
   credentials: true,
    methods: ["GET","POST","DELETE","PUT","OPTIONS"],
    allowedHeaders: ["Content-Type", "Authorization"] 
