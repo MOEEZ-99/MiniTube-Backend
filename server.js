7d91141b4e43cdb9a0c35415e8c4d169d084637c
Abdul Moeez <stuntsadv0987654@gmail.com>
2025-08-30 22:50:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -18,13 +18,34 @@ app.use((req, res, next) => {
   next();
 });
 
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mini-tube-123.netlify.app"
+];
 
+// Apply CORS first
 app.use(cors({
-  origin: "https://mini-tube-123.netlify.app",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-   methods: ["GET","POST","DELETE","PUT","OPTIONS"],
-   allowedHeaders: ["Content-Type", "Authorization"] 
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+// Force preflight handling
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  return res.sendStatus(200);
+});
 
 app.get('/test', (req, res) => {
   res.status(200).send('CORS is working');
