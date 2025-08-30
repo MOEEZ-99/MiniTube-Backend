2a70b0bd1b465cf11ed56cc2ce949016917a164b
Abdul Moeez <stuntsadv0987654@gmail.com>
2025-08-30 21:48:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,38 @@ import playlistRoutes from "./routes/playlist.routes.js";
 
 const app = express();
 
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+//    methods: ["GET","POST","DELETE","PUT","OPTIONS"],
+//    allowedHeaders: ["Content-Type", "Authorization"] 
+// }));
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://minitube-frontend.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-   methods: ["GET","POST","DELETE","PUT","OPTIONS"],
-   allowedHeaders: ["Content-Type", "Authorization"] 
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
